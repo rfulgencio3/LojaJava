cc92f4874d2d27210bdf3f373dd25d3646665a0a
Ricardo Fulgencio Alves <ricardo.alves@socialbank.com.br>
2021-07-30 07:47:27 -0300

    Implementando o padrao Observer no package pedido.acao utilizando a classe AcaoAposGerarPedido.

--- src/TestesPedidos.java
+++ src/TestesPedidos.java
@@ -2,18 +2,36 @@ import orcamento.Orcamento;
 import pedido.GeraPedido;
 import pedido.GeraPedidoHandler;
 import pedido.Pedido;
-
+import pedido.acao.EnviarEmailPedido;
+import pedido.acao.SalvarPedidoNoBancoDeDados;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class TestesPedidos {
     public static void main(String[] args) {
-        String cliente = args[0];
-        BigDecimal valorOrcamento = new BigDecimal(args[1]);
-        int quantidadeItens = Integer.parseInt(args[2]);
+        String cliente;
+        BigDecimal valorOrcamento;
+        int quantidadeItens = Integer.parseInt("4");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Client name: ");
+        cliente = input.next();
+
+        System.out.print("Budget value: ");
+        valorOrcamento = input.nextBigDecimal();
+
+        System.out.print("Quantity: ");
+        quantidadeItens = input.nextInt();
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-        GeraPedidoHandler handler = new GeraPedidoHandler(/* Dependências */);
+        GeraPedidoHandler handler = new GeraPedidoHandler(
+                Arrays.asList(
+                    new SalvarPedidoNoBancoDeDados(),
+                    new EnviarEmailPedido()
+                ));
         handler.execute(gerador);
     }
 }
